3cd5d9352dfef21a24f4f159be81679248c074b5
Bailey Sostek <bhsostek@wpi.edu>
2021-01-29 23:03:47 -0500

    Added Documentation

--- index.js
+++ index.js
@@ -1,46 +1,86 @@
+// Import the discord.js library. 
+// Documentation :
+// https://discord.js.org/#/docs/main/stable/general/welcome
 const Discord = require("discord.js");
+
+// Config file, this is where our private environment variables are stored.
 const config = require("./config.json");
 
+// Get a handle to the client
 const client = new Discord.Client();
 
+// Get a handle to all of the channels in this server.
 const channelManager = client.channels;
 
 const prefix = "!";
 
-client.on("message", (message) => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+// This is where we define our list of commands. This object is used as a hashmap where each key is a command name mapped to a callback function to execute when the command is entered.
+// Example "goto":(args) => { console.log(args[1]) }
+// You can access a commands callback function by typing COMMANDS[commandName] eg. COMMANDS["goto"]
+let COMMANDS = {};
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
+/*
+  Add a command
+*/
+addCommand = (commandName, commandCallback) => {
+  //Add a new Command to the COMMANDS array with the key of "commandName"
+  COMMANDS[commandName] = commandCallback;
+}
 
-  if (command === "goto") {
-    const room = args[0];
 
-    let channels = message.guild.channels.cache;
-    
-    let channelID = searchForChannel(room, channels);
-    if(channelID){
-      message.reply("Sending user to room:" + room);
-      message.guild.member(message.author.id).voice.setChannel(channelID);
-    }else{
-      let names = "";
+//------------------------------------------------------------------------
+//                   Register Command Handlers Here
+//------------------------------------------------------------------------
+addCommand("goto", (args) => {
+  const room = args[0];
 
-      for(test of channels){
-        let channel = test[1];
-        names += channel.name + " ,"
-      }
-      names = names.substring(0, names.length -2);
+  let channels = message.guild.channels.cache;
+  
+  let channelID = searchForChannel(room, channels);
+  if(channelID){
+    message.reply("Sending user to room:" + room);
+    message.guild.member(message.author.id).voice.setChannel(channelID);
+  }else{
+    let names = "";
 
-      message.reply("Sorry that room does not exist in this house. The only rooms in this house are: [" + names + "]");
+    for(test of channels){
+      let channel = test[1];
+      names += channel.name + " ,"
     }
-  }
+    names = names.substring(0, names.length -2);
 
-  if (command === "c") {
-    console.log(channelManager.cache);
+    message.reply("Sorry that room does not exist in this house. The only rooms in this house are: [" + names + "]");
   }
 });
+//------------------------------------------------------------------------
+
+
+// Here is where we start processing hooks. Any time a message is sent it will hit this hook, and the content of message will be passed as the "message" paramiter of the following function.
+// (message) => {} is a first class object of type function. This is a way to pass a function as a paramiter.
+client.on("message", (message) => {
+  //If the author of the message was the bot. Dont process the message just return.
+  if (message.author.bot) return;
+  //If this message does not start with our prefix, "!" return
+  if (!message.content.startsWith(prefix)) return;
+
+  //Parse the body, remove the prefix
+  const commandBody = message.content.slice(prefix.length);
+  //Each command argument is separated by a space. args will be an array.
+  const args = commandBody.split(' ');
+  //Command is args[0].toLowerCase
+  const command = args.shift().toLowerCase();
+
+  //See if we have an entry for this Command
+  //This sees if there is a Key on COMMANDS named command, returns true if the key exists.
+  if(COMMANDS[command]){
+    //adding parens at the end of dereferencing the array evalues the function stored in the COMMANDS array at "command" with the parameters args.
+    COMMANDS[command](args);
+  }
+
+});
+
+
+
 
 /*
   Pass this a string Name and a list of the channels. This returns the unique Channel ID to connect to or false.
