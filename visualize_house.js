7ef5b8dd041bc81df059063851ea0bfed484a334
Bromingo <domingobrown1@gmail.com>
2021-01-31 00:25:17 -0500

    testing graph stuff

--- visualize_house.js
+++ visualize_house.js
@@ -1,6 +1,7 @@
-/* global fetch, cytoscape, document, window */
+/* global fetch, cytoscape, document, window, require */
 
 (function(){
+  var toJson = function(res){ return res.json(); };
     window.cy = cytoscape({
       container: document.getElementById('cy'),
   
@@ -9,31 +10,27 @@
         cols: 3
       },
   
-      // style: fetch('cy-style.json').then(function(res){
-      //   return res.json();
-      // }),
+      style: [ // the stylesheet for the graph
+        {
+          selector: 'node',
+          style: {
+            'background-color': '#666',
+            'label': 'data(id)'
+          }
+        },
+    
+        {
+          selector: 'edge',
+          style: {
+            'width': 3,
+            'line-color': '#ccc',
+            'target-arrow-color': '#ccc',
+            'target-arrow-shape': 'triangle',
+            'curve-style': 'bezier'
+          }
+        }
+      ],
   
-      elements: [
-        { data: { label: 'top left' }, classes: 'top-left' },
-        { data: { label: 'top center' }, classes: 'top-center' },
-        { data: { label: 'top right' }, classes: 'top-right' },
-  
-        { data: { label: 'center left' }, classes: 'center-left' },
-        { data: { label: 'center center' }, classes: 'center-center' },
-        { data: { label: 'center right' }, classes: 'center-right' },
-  
-        { data: { label: 'bottom left' }, classes: 'bottom-left' },
-        { data: { label: 'bottom center' }, classes: 'bottom-center' },
-        { data: { label: 'bottom right' }, classes: 'bottom-right' },
-  
-        { data: { label: 'multiline manual\nfoo\nbar\nbaz' }, classes: 'multiline-manual' },
-        { data: { label: 'multiline auto foo bar baz' }, classes: 'multiline-auto' },
-        { data: { label: 'outline' }, classes: 'outline' },
-  
-        { data: { id: 'ar-src' } },
-        { data: { id: 'ar-tgt' } },
-        { data: { source: 'ar-src', target: 'ar-tgt', label: 'autorotate (move my nodes)' }, classes: 'autorotate' },
-        { data: { label: 'background' }, classes: 'background' }
-      ]
+      elements: fetch('test.json').then(toJson)
     });
   })();
